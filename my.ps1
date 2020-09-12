d6bf9412434f60434eeb141ba23565786e00140e
Rogerio Coimbra <rogeriocoimbra@gmail.com>
2020-09-12 19:57:39 -0300

    Insert npm commands

--- my.ps1
+++ my.ps1
@@ -1,23 +1,28 @@
 $Parametro01 = $args[0]
 $Parametro02 = $args[1]
+$Parametro03 = $args[2]
 
 if (!$Parametro01){
     Write-Host("Commands:");
-    Write-Host("my      : Help");
-    Write-Host("my vm   : ViewMemory");
-    Write-Host("my sh   : ScanHealth");
-    Write-Host("my cd   : CheckDisk");
-    Write-Host("my d    : Defrag");
+    Write-Host("my       : Help");
+    Write-Host("my vm    : ViewMemory");
+    Write-Host("my sh    : ScanHealth");
+    Write-Host("my cd    : CheckDisk");
+    Write-Host("my d     : Defrag");
     Write-Host("");
 	Write-Host("Projetos:");
-    Write-Host("my p    : Projetos");
-	Write-Host("my c    : Commands");
-    Write-Host("my a9   : Angular9");
-	Write-Host("my a9 b : Angular9 BackEnd");
-	Write-Host("my a9 f : Angular9 FrontEnd");
-    #Write-Host("my m    : Maestro");
-	#Write-Host("my m s  : Maestro Sdk");
-	#Write-Host("my m s  : Maestro WebApp");
+    Write-Host("my p     : Projetos");
+	Write-Host("my c     : Commands");
+    Write-Host("my a9    : Angular9");
+	Write-Host("my a9 b  : Angular9 BackEnd");
+	Write-Host("my a9 f  : Angular9 FrontEnd");
+    #Write-Host("my m     : Maestro");
+	#Write-Host("my m s   : Maestro Sdk");
+	#Write-Host("my m s i : Maestro Sdk Instalar");
+	#Write-Host("my m s a : Maestro Sdk Atualizar");
+	#Write-Host("my m w   : Maestro WebApp");
+	#Write-Host("my m w i : Maestro WebApp Instalar");
+	#Write-Host("my m w a : Maestro WebApp Atualizar");
     Write-Host("");
 }
 
@@ -46,10 +51,30 @@ if ($Parametro01 -eq "a9"){
 if ($Parametro01 -eq "m"){
 	if ($Parametro02 -eq "s"){
 		Set-Location("C:\Projetos\carrefour-maestro-source\FONTES DEV\Sdk");
+		
+		if ($Parametro03 -eq "i"){
+			Write-Host("npm run InstalarMaestro");
+			npm run InstalarMaestro
+		}
+		
+		if ($Parametro03 -eq "a"){
+			Write-Host("npm run AtualizarMaestro");
+			npm run AtualizarMaestro
+		}
 	}
 
 	if ($Parametro02 -eq "w"){
 		Set-Location("C:\Projetos\carrefour-maestro-source\FONTES DEV\webApp");
+		
+		if ($Parametro03 -eq "i"){
+			Write-Host("npm run InstalarMaestro");
+			npm run InstalarMaestro
+		}
+		
+		if ($Parametro03 -eq "a"){
+			Write-Host("npm run AtualizarMaestro");
+			npm run AtualizarMaestro
+		}
 	}
 
 	if (!$Parametro02){
@@ -98,16 +123,16 @@ if ($Parametro01 -eq "cd"){
     Write-Host("                               Verificando C:");
     Write-Host("****************************************************************************");
     chkdsk C: /scan
-    Write-Host("");
-    Write-Host("****************************************************************************");
-    Write-Host("                               Verificando D:");
-    Write-Host("****************************************************************************");
-    chkdsk D: /scan
-    Write-Host("");
-    Write-Host("****************************************************************************");
-    Write-Host("                               Verificando E:");
-    Write-Host("****************************************************************************");
-    chkdsk E: /scan
+    #Write-Host("");
+    #Write-Host("****************************************************************************");
+    #Write-Host("                               Verificando D:");
+    #Write-Host("****************************************************************************");
+    #chkdsk D: /scan
+    #Write-Host("");
+    #Write-Host("****************************************************************************");
+    #Write-Host("                               Verificando E:");
+    #Write-Host("****************************************************************************");
+    #chkdsk E: /scan
 }
 
 if ($Parametro01 -eq "d"){
@@ -117,14 +142,14 @@ if ($Parametro01 -eq "d"){
     Write-Host("                               Desfragmentando C:");
     Write-Host("****************************************************************************");
     Defrag C: /U /V /D /O
-    Write-Host("");
-    Write-Host("****************************************************************************");
-    Write-Host("                               Desfragmentando D:");
-    Write-Host("****************************************************************************");
-    Defrag D: /U /V /D /O
-    Write-Host("");
-    Write-Host("****************************************************************************");
-    Write-Host("                               Desfragmentando E:");
-    Write-Host("****************************************************************************");
-    Defrag E: /U /V /D /O
+    #Write-Host("");
+    #Write-Host("****************************************************************************");
+    #Write-Host("                               Desfragmentando D:");
+    #Write-Host("****************************************************************************");
+    #Defrag D: /U /V /D /O
+    #Write-Host("");
+    #Write-Host("****************************************************************************");
+    #Write-Host("                               Desfragmentando E:");
+    #Write-Host("****************************************************************************");
+    #Defrag E: /U /V /D /O
 }
